0e51770af01ff6e1bd966cb6981915dd4c0f076b
Jesulayomy <jesulayomi081@gmail.com>
2025-04-21 08:26:06 -0400

    [Auth]: Thumbs Down functionality

--- rc_basics/week10-day7/savage-auth/public/main.js
+++ rc_basics/week10-day7/savage-auth/public/main.js
@@ -1,4 +1,5 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
 var trash = document.getElementsByClassName("fa-trash");
 
 Array.from(thumbUp).forEach(function(element) {
@@ -12,7 +13,31 @@ Array.from(thumbUp).forEach(function(element) {
           body: JSON.stringify({
             'name': name,
             'msg': msg,
-            'thumbUp':thumbUp
+            'thumbUp':thumbUp + 1
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(thumbDown).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        fetch('messages', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbUp': thumbUp > 0 ? thumbUp - 1 : 0
           })
         })
         .then(response => {
